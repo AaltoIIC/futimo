f1ef12169f40c0ffe700006f850aa98f217596f7
Riku Ala-Laurinaho <riku.ala-laurinaho@aalto.fi>
2022-06-28 10:32:49 +0300

    fixes

--- expand_fuzzy_sets_for_visualization.py
+++ expand_fuzzy_sets_for_visualization.py
@@ -3,19 +3,19 @@ import functools
 from fuzzy_modeling import read_fuzzy_sets, aggregate_data_with_query
 
 # File names
-FILE_NAME_FUZZY_SETS = "examples/fuzzy_sets.txt" #Definitions for fuzzy sets
+FILE_NAME_FUZZY_SETS = "examples/fuzzy_sets_crane.txt" #Definitions for fuzzy sets
 FILE_NAME_DATA_BASE = "fuzzy_data.db" #Database location
 OUTPUT_FILE = "data_for_visualization.csv"
 
 # Select csv file delimiter
-CSV_DELIMITER = ";"
+CSV_DELIMITER = ","
 
 # Aggregate data
 AGGREGATE_DATA = True #Fetch aggregated data from database
 
-LIST_OF_AGGREGATED_VARIABLES = ["Temperature", "Voltage"] #The order of variables MUST BE same as in the database/fuzzy sets description file
+LIST_OF_AGGREGATED_VARIABLES = ["BridgePosition", "TrolleyPosition"] #The order of variables MUST BE same as in the database/fuzzy sets description file
 #LIST_OF_AGGREGATED_VARIABLES = ["BridgePosition", "LoadTare", "HoistPosition", "TrolleyPosition"] #["Temperature", "Voltage", "Motor speed rpm", "AlertOn", "Variable 5"] #Select aggregated variables
-TABLE_NAME = "fuzzy_sets"
+TABLE_NAME = "crane_data"
 
 def construct_output_file(data, fuzzy_sets, outputfile, delimiter, list_of_variables):
     with open(outputfile, "w") as f:
@@ -27,8 +27,6 @@ def construct_output_file(data, fuzzy_sets, outputfile, delimiter, list_of_varia
             data_row = []
             for i in range(len(row) - 1):
                 ordinal_number = row[i]
-                print("ordinal", ordinal_number)
-                print(fuzzy_sets[i][1][ordinal_number - 1][:2])
                 mean, sigma = fuzzy_sets[i][1][ordinal_number - 1][:2]
                 data_row.extend([mean, sigma])
 
@@ -42,7 +40,6 @@ def main():
     fuzzy_sets = list(filter(lambda fuzzy_set: fuzzy_set[0] in LIST_OF_AGGREGATED_VARIABLES, fuzzy_sets))
     if AGGREGATE_DATA:
         data = aggregate_data_with_query(LIST_OF_AGGREGATED_VARIABLES, FILE_NAME_DATA_BASE, TABLE_NAME)
-        print(data)
     construct_output_file(data, fuzzy_sets, OUTPUT_FILE, CSV_DELIMITER, LIST_OF_AGGREGATED_VARIABLES)
     
 if __name__ == "__main__":
